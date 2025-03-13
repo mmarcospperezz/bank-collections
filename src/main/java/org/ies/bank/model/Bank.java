fec0a4bb9c47a39199f05e93b22d362b31d7f88f
mmarcospperezz <mmarcospperez2204@gmail.com>
2025-03-13 10:57:44 +0100

    bank

--- src/main/java/org/ies/bank/model/Bank.java
+++ src/main/java/org/ies/bank/model/Bank.java
@@ -12,4 +12,14 @@ public class Bank {
     private String name;
     private List<Customer> customers;
     private Map<String, Account> accountsByIban;
+
+
+    public void addAmount(String iban, double amount) {
+        if (accountsByIban.containsKey(iban)) {
+            Account account = accountsByIban.get(iban);
+            account.setSaldo(account.getSaldo() + amount);
+        } else {
+            System.out.println("No existe la cuenta");
+        }
+    }
 }
