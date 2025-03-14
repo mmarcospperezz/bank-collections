dc2ae8cdc8f8ced6083f8ac98124b04d79243f56
mmarcospperezz <mmarcospperez2204@gmail.com>
2025-03-14 13:40:59 +0100

    bank

--- src/main/java/org/ies/bank/model/Bank.java
+++ src/main/java/org/ies/bank/model/Bank.java
@@ -27,24 +27,41 @@ public class Bank {
     }
 
     public List<Account> getAccountsByCustomerNif(String nif) {
-        boolean customerExists = false;
+        if (!isCustomerExists(nif)) {
+            return null;
+        } else {
+            List<Account> customerAccounts = new ArrayList<>();
+            for (Account account : accountsByIban.values()) {
+                if (account.getNif().equals(nif)) {
+                    customerAccounts.add(account);
+                }
+            }
+            return customerAccounts;
+        }
+    }
+
+    private boolean isCustomerExists(String nif) {
         for (Customer customer : customers) {
             if (customer.getNif().equals(nif)) {
-                customerExists = true;
+                return true;
             }
         }
-
-        if (!customerExists) {
-            return null;
-        }
-
-        List<Account> customerAccounts = new ArrayList<>();
-        for (Account account : accountsByIban.values()) {
-            if (account.getNif().equals(nif)) {
-                customerAccounts.add(account);
-            }
-        }
-        return customerAccounts;
+        return false;
     }
+
+    public void withdrawAmount(String iban, double amount) {
+        if (!accountsByIban.containsKey(iban)) {
+            System.out.println("No existe la cuenta");
+            return;
+        }
+
+        Account account = accountsByIban.get(iban);
+        if (account.getSaldo() < amount) {
+            System.out.println("No hay saldo suficiente");
+        } else {
+            account.setSaldo(account.getSaldo() - amount);
+        }
+    }
+
 }
 
